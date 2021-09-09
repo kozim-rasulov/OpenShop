30430dc15c455fc608ce92544516eec1606384b3
kozim-rasulov <kozim_7484443@mail.ru>
2021-09-09 16:31:38 +0500

    pages distributed

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1200) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
+    });
+
+    /* $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    }); */
+});
