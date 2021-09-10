babcdbc44e30035eb91044e40a2c0e62cd656e8d
kozim-rasulov <kozim_7484443@mail.ru>
2021-09-10 11:32:48 +0500

    link up added

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1200) {
-            $('.page-up').fadeIn();
-        } else {
-            $('.page-up').fadeOut();
-        }
-    });
 
-    /* $("a[href^='#']").click(function(){
-        const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-        return false;
-    }); */
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1200) {
+        $('.page-up').fadeIn();
+    } else {
+        $('.page-up').fadeOut();
+    }
 });
+
+/* $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+}); */
