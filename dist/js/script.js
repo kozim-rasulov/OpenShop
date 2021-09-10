beaa695cd80d32ddc2a0e38a0adf362b3c3356ac
kozim-rasulov <kozim_7484443@mail.ru>
2021-09-10 16:57:05 +0500

    ready beta

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,9 +6,3 @@ $(window).scroll(function() {
         $('.page-up').fadeOut();
     }
 });
-
-/* $("a[href^='#']").click(function(){
-    const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    return false;
-}); */
